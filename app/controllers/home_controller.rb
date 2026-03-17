6b397a337d04c41fe31850d16b2303012fe9d1a9
Guilherme <guiyonesnogara@gmail.com>
2026-03-17 10:53:29 -0300

    atualizei a logo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,23 +3,30 @@ class HomeController < ApplicationController
     @active_challenges = Current.user.challenges
                                      .where(status: "active", quest_id: nil)
                                      .order(created_at: :desc)
+    
     @active_quests = Current.user.quests
                                   .where(status: "active")
                                   .includes(:challenges, :reward)
                                   .order(created_at: :desc)
+    
     @unlocked_rewards = Current.user.rewards
                                     .where(status: "unlocked")
                                     .includes(:challenge, :quest)
+    
     @locked_rewards = Current.user.rewards
                                    .where(status: "locked")
                                    .includes(:challenge, :quest)
+    
     @streak = Current.user.current_streak
+    
     @active_count = Current.user.active_challenges_count
+    
     @unlocked_count = Current.user.unlocked_rewards_count
 
     @today_challenges = Current.user.challenges
                                     .where(status: "active")
                                     .order(created_at: :asc)
+    
     @today_checkins = Checkin.where(
       challenge_id: @today_challenges.map(&:id),
       created_at: Date.today.all_day
